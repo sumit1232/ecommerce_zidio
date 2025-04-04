53de7cfc8194924058d42418cc5fe152575c22ac
Sumit Jadhav <sumitjadav2525@gmail.com>
2025-04-04 14:08:18 +0530

    login and register routes and and components

--- frontend/ecommerceApp/src/App.jsx
+++ frontend/ecommerceApp/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './components/Login'
+import Register from './components/Register'
+import Home from './components/Home';
 
 function App() {
   return (
     <div>
-        <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+       <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/registration' element={<Register />}/>
+       
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
